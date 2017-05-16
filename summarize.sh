a781fa6b5431b502edfd1c60691962dd2f3ee754
Richard Neil Ilagan <richard.ilagan@hpe.com>
2017-05-16 11:27:21 +0100

    Gets filename of last DTX definition file

---
+++ summarize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# :: retrieve a single DTX definition for use with meta
+function get_dtx {
+  local dtx
+  local set_definition=$1
+
+  while read line
+  do
+    if [[ $line =~ ^#L[0-9]FILE\:\s*(.*)$ ]]; then
+      dtx=${BASH_REMATCH[1]}
+    fi
+  done <<< "$(iconv -f SHIFT-JIS -t UTF-8 "$set_definition")"
+
+  echo $dtx
+  echo "###"
+}
+
+# :: get all set.def files
+find . -type f -name "set.def" -print0 | \
+while IFS= read -r -d '' file; do
+  get_dtx "$file";
+done
